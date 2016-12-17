1915666943388fb22cc32a651fe91cba8458fb18
jaywsj <wangshijiejay@gmail.com>
2016-12-17 16:05:38 +0800

    update TieZiViewModel.m

--- yingwo/Home/ViewModel/TieZiViewModel.m
+++ yingwo/Home/ViewModel/TieZiViewModel.m
@@ -502,6 +502,34 @@
           }];
 }
 
+- (void)requestForLoginStatusWithUrl:(NSString *)url
+                          paramaters:(NSDictionary *)paramaters
+                             success:(void (^)(StatusEntity *statusEntity))success
+                             failure:(void (^)(NSString *error))failure{
+    
+    NSString *fullUrl      = [BASE_URL stringByAppendingString:url];
+    YWHTTPManager *manager =[YWHTTPManager manager];
+    
+    [manager POST:fullUrl
+       parameters:paramaters
+         progress:nil
+          success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+              
+              NSHTTPURLResponse *httpResponse = (NSHTTPURLResponse *)task.response;
+              if (httpResponse.statusCode == SUCCESS_STATUS) {
+                  NSDictionary *content = [NSJSONSerialization JSONObjectWithData:responseObject
+                                                                          options:NSJSONReadingMutableContainers
+                                                                            error:nil];
+                  StatusEntity *entity       = [StatusEntity mj_objectWithKeyValues:content[@"info"]];
+                  success(entity);
+              }
+              
+          } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+              
+          }];
+}
+
+
 
 - (void)postTieZiLIkeWithUrl:(NSString *)url
                   paramaters:(NSDictionary *)paramaters
